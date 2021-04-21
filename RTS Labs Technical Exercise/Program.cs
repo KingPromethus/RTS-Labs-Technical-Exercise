2eb78444ce0333979082e8ae479a0977e5839449
Marc C <KingPromethus@users.noreply.github.com>
2021-04-20 21:50:10 -0400

    Add project files.

---
+++ RTS Labs Technical Exercise/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace RTS_Labs_Technical_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            int[] testArray = new int[] { 1, 5, 2, 1, 10 };
+            System.Console.WriteLine(program.AboveBelow(testArray, 6));
+            testArray = new int[] { 2, 3, 5, 12, 51, 23, 21 };
+            System.Console.WriteLine(program.AboveBelow(testArray, 5));
+
+            
+            for(int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine(program.RotateString("MyString", i));
+            }
+
+            System.Console.WriteLine("\n I don't have a lot of extensive experience with languages to the point where I know something I really dislike off the top of my head. I will say I do wish Visual Studio had line-item reverts and local git history like IntelliJ does.");
+        }
+
+        public string AboveBelow(int[] inputArray, int inputInt)
+        {
+            int above, below;
+            above = below = 0;
+            foreach (int number in inputArray)
+            {
+                if (number > inputInt)
+                    below++;
+                else if (number < inputInt)
+                    above++;
+            }
+            return $"Above: {above}, Below: {below}";
+        }
+
+
+        public string RotateString(string inputString, int inputInt)
+        {
+            if (inputInt <= inputString.Length && inputInt > 0)
+            {
+                return inputString.Substring(inputString.Length - inputInt) + inputString.Remove(inputString.Length - inputInt);
+            }
+            else if(inputInt > inputString.Length)
+            {
+                return inputString.Substring(inputString.Length - (inputInt % inputString.Length)) + inputString.Remove(inputString.Length-(inputInt % inputString.Length));
+            }
+            else
+            {
+                return inputString;
+            }
+        }
+    }
+}
