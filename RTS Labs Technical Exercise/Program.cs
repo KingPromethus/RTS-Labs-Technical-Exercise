ec771dc77403b06b927660f7435f1123fd3b08fe
Marc C <KingPromethus@users.noreply.github.com>
2021-05-02 11:29:58 -0400

    CodeMaid re-order

--- RTS Labs Technical Exercise/Program.cs
+++ RTS Labs Technical Exercise/Program.cs
@@ -1,26 +1,7 @@
-﻿using System;
-
-namespace RTS_Labs_Technical_Exercise
+﻿namespace RTS_Labs_Technical_Exercise
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            Program program = new Program();
-            int[] testArray = new int[] { 1, 5, 2, 1, 10 };
-            System.Console.WriteLine(program.AboveBelow(testArray, 6));
-            testArray = new int[] { 2, 3, 5, 12, 51, 23, 21 };
-            System.Console.WriteLine(program.AboveBelow(testArray, 5));
-            System.Console.WriteLine();
-            
-            for(int i = 0; i < 10; i++)
-            {
-                System.Console.WriteLine(program.RotateString("MyString", i));
-            }
-
-            System.Console.WriteLine("\nI don't have a lot of extensive experience with languages to the point where I know something I really dislike off the top of my head. I will say I do wish Visual Studio had line-item reverts and local git history like IntelliJ does. Currently your only option is to revert entire files and if you do, you cannot get the files your revert back if you decide against your initial revert.");
-        }
-
         public string AboveBelow(int[] inputArray, int inputInt)
         {
             int above, below;
@@ -35,21 +16,37 @@ namespace RTS_Labs_Technical_Exercise
             return $"Above: {above}, Below: {below}";
         }
 
-
         public string RotateString(string inputString, int inputInt)
         {
             if (inputInt <= inputString.Length && inputInt > 0)
             {
                 return inputString.Substring(inputString.Length - inputInt) + inputString.Remove(inputString.Length - inputInt);
             }
-            else if(inputInt > inputString.Length)
+            else if (inputInt > inputString.Length)
             {
-                return inputString.Substring(inputString.Length - (inputInt % inputString.Length)) + inputString.Remove(inputString.Length-(inputInt % inputString.Length));
+                return inputString.Substring(inputString.Length - (inputInt % inputString.Length)) + inputString.Remove(inputString.Length - (inputInt % inputString.Length));
             }
             else
             {
                 return inputString;
             }
+        }
+
+        private static void Main(string[] args)
+        {
+            Program program = new Program();
+            int[] testArray = new int[] { 1, 5, 2, 1, 10 };
+            System.Console.WriteLine(program.AboveBelow(testArray, 6));
+            testArray = new int[] { 2, 3, 5, 12, 51, 23, 21 };
+            System.Console.WriteLine(program.AboveBelow(testArray, 5));
+            System.Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine(program.RotateString("MyString", i));
+            }
+
+            System.Console.WriteLine("\nI don't have a lot of extensive experience with languages to the point where I know something I really dislike off the top of my head. I will say I do wish Visual Studio had line-item reverts and local git history like IntelliJ does. Currently your only option is to revert entire files and if you do, you cannot get the files your revert back if you decide against your initial revert.");
         }
     }
 }
